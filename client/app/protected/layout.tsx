9a29d16c60be81876d6ba88c45c06ac00d259f47
ribhavsharma <ribhavsharma2003@gmail.com>
2025-11-22 01:13:01 -0800

    migrate to supabase, auth complete

--- client/app/protected/layout.tsx
+++ client/app/protected/layout.tsx
@@ -1,10 +1,13 @@
 import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import Link from "next/link";
 import { Suspense } from "react";
 
-export default function Home() {
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -19,9 +22,7 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <main className="flex-1 flex flex-col gap-6 px-4">
-           home page stuff
-          </main>
+          {children}
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
